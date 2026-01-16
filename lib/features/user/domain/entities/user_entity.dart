6ec321665e0c6b1be1c347b2b835034c7a28b2b5
Dipen <lamadipen9953@gmail.com>
2026-01-16 12:08:46 +0545

    added user entity in features

---
+++ lib/features/user/domain/entities/user_entity.dart
@@ -0,0 +1,12 @@
+
+
+class BatchEntity extends Equatable {
+  final String? userId;
+  final String username;
+  final String? status;
+
+  const BatchEntity({this.userId, required this.username, this.status});
+
+  @override
+  List<Object?> get props => [userId, username, status];
+}
